ec13311c3c875e9f60440d61bb71c4e23f0ade0f
Polina <74186092+polinkasosiska@users.noreply.github.com>
2024-03-01 02:05:02 +0300

    Update component_test.py

--- shopping_cart/component_test.py
+++ shopping_cart/component_test.py
@@ -3,17 +3,24 @@ from flask import Flask
 import asyncio
 import requests
 
+# Базовый URL для тестирования
 base_url = "http://localhost:8001/"
-# все же компонент тест
 
 def test_get_card():
+    # Тест для проверки GET-запроса к карте
     response = requests.get(f"{base_url}cart")
+    # Отправка GET-запроса к конечной точке "cart"
     print(response)
+    # Вывод ответа для отладки
     assert response.status_code == 200
+    # Проверка, что статус ответа равен 200 (успешно)
 
 
 def test_post_card():
+    # Тест для проверки POST-запроса к карте
     response = requests.post(
         f"{base_url}cart", json={"id": 1, "name": "test", "price": 123}
     )
+    # Отправка POST-запроса с данными карты
     assert response.status_code == 201
+    # Проверка, что статус ответа равен 201 (создано)
