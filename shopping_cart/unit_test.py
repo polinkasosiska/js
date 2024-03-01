ffc62cbea60d5600bbcfe63ec92c321c1eaf2f38
Polina <74186092+polinkasosiska@users.noreply.github.com>
2024-03-01 03:20:07 +0300

    Update unit_test.py

--- shopping_cart/unit_test.py
+++ shopping_cart/unit_test.py
@@ -1,39 +1,23 @@
 import pytest
-from flask_testing import TestCase
-from shopping_cart import app, cart
+from flask import Flask
+from shopping_cart import (
+    app,
+)  # интеграционный тест, тк он проверяет взаимодействие между различными частями приложения Flask, включая обработку HTTP-запросов и ответов.
 
-class ShoppingCartTest(TestCase):
-    def create_app(self):
-        app.config['TESTING'] = True
-        return app
 
-def test_get_cart_empty(self):
-    response = self.client.get("/cart")
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_get_card(client):
+    response = client.get("/cart")
     assert response.status_code == 200
-    assert response.get_json() == []
 
-def test_add_to_cart(self):
-    item = {"id": 1, "name": "Necklace", "price": 100}
-    response = self.client.post("/cart", json=item)
+
+def test_post_card(client):
+    response = client.post("/cart", json={"id": 1, "name": "test", "price": 123})
     assert response.status_code == 201
-    assert response.get_json() == {"message": "Item added to cart"}
-    assert cart == [item]
-
-def test_clear_cart(self):
-    cart.append({"id": 1, "name": "Necklace", "price": 100})
-    response = self.client.delete("/cart")
-    assert response.status_code == 200
-    assert response.get_json() == {"message": "Cart cleared"}
-    assert cart == []
-
-def test_remove_from_cart(self):
-    cart.append({"id": 1, "name": "Necklace", "price": 100})
-    response = self.client.delete("/cart/1")
-    assert response.status_code == 200
-    assert response.get_json() == {"message": "Item removed from cart"}
-    assert cart == []
-
-def test_remove_non_existing_item(self):
-    response = self.client.delete("/cart/1")
-    assert response.status_code == 404
-    assert response.get_json() == {"error": "Item not found in cart"}
+    
